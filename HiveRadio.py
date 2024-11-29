672537b6c6810dd8dd737a47081f4d2ad304bd31
Lushki <eladhelfman@gmail.com>
2024-11-29 15:15:34 +0200

    Added MusicHandler.py & PlaylistHandler.py

---
+++ HiveRadio.py
@@ -0,0 +1,13 @@
+# Hive Radio
+import MusicHandler
+import PlayListHandler
+
+
+def main():
+    print("***************************Hive*Radio***************************")
+
+    print("****************************************************************")
+
+
+if __name__ == "__main__":
+    main()
